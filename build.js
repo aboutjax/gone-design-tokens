b5d042585a7336e32763a30bf276b72e4a0e7b6e
Che Wei (Jacky) Lee <aboutjax1990@gmail.com>
2023-12-26 02:16:21 -0500

    update

--- build.js
+++ build.js
@@ -1,50 +1,64 @@
-const StyleDictionary = require("style-dictionary");
-const { registerTransforms } = require("@tokens-studio/sd-transforms");
+import { registerTransforms } from "@tokens-studio/sd-transforms";
+import StyleDictionary from "style-dictionary";
+import { ColorTranslator } from "colortranslator";
 
 // sd-transforms, 2nd parameter for options can be added
 // See docs: https://github.com/tokens-studio/sd-transforms
-registerTransforms(StyleDictionary, {
-  expand: { composite: true, typography: true, shadow: true },
-  excludeParentKeys: false,
-});
-const { promises } = require("fs");
+registerTransforms(StyleDictionary);
 
-// // example value transform, which just returns the token as is
-StyleDictionary.registerFilter({
-  name: "my-filter",
-  matcher: (token) => {
-    console.log(token);
-    return token.filePath !== "tokens/core.json";
-  },
-});
-
-async function run() {
-  const $themes = JSON.parse(
-    await promises.readFile("tokens/$themes.json", "utf-8")
-  );
-  const configs = $themes.map((theme) => ({
-    source: Object.entries(theme.selectedTokenSets)
-      .filter(([, val]) => val !== "disabled")
-      .map(([tokenset]) => `tokens/${tokenset}.json`),
+[
+  "light",
+  "dark",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "navy",
+  "purple",
+].map((theme) => {
+  const sd = StyleDictionary.extend({
+    source: [`transformed/${theme}-tokens.json`],
     platforms: {
       css: {
-        transformGroup: "tokens-studio",
+        // transformGroup: "tokens-studio",
+        transforms: ["attribute/cti", "name/cti/kebab", "test/color/rgb"],
+        buildPath: "build/css/",
         files: [
           {
-            destination: `build/css/vars-${theme.name}.css`,
-            format: "css/variables",
-            filter: "my-filter",
+            destination: `variables-${theme}.css`,
+            format: "css/variables/gone",
+            selector: `.theme-${theme}`,
           },
         ],
       },
     },
-  }));
-
-  configs.forEach((cfg) => {
-    const sd = StyleDictionary.extend(cfg);
-    sd.cleanAllPlatforms(); // optionally, cleanup files first..
-    sd.buildAllPlatforms();
   });
-}
 
-run();
+  sd.registerTransform({
+    name: "test/color/rgb",
+    type: "value",
+    matcher: function (token) {
+      return token.attributes.category === "color";
+    },
+    transformer: function (token) {
+      const rgbo = new ColorTranslator(token.value).RGBObject;
+      const string = `${rgbo.R} ${rgbo.G} ${rgbo.B}`;
+      return string;
+    },
+  });
+
+  sd.registerFormat({
+    name: "css/variables/gone",
+    formatter: function (dictionary) {
+      return `${this.selector} {
+        ${dictionary.allProperties
+          .map((prop) => `  --${prop.name}: ${prop.value};`)
+          .join("\n")}
+      }`;
+    },
+  });
+
+  sd.cleanAllPlatforms();
+  sd.buildAllPlatforms();
+});
